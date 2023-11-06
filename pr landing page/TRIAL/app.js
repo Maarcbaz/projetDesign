458a4f6d4a8666df156ab652e19f17c8f34f8683
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2023-11-06 16:27:27 +0100

    pr commit

---
+++ pr landing page/TRIAL/app.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").onclick = function () {
+    alert("i'm a dialogue box")
+};
+
+function myFunc() {
+    document.getElementById("element").innerHTML = "yay!"
+};
+function myFunc() {
+    document.getElementById("image").src = "../image/CERTIFICATE.png"
+}
+function mypic() {
+    document.getElementById("image").src = "../image/logo8.jpeg"
+}
